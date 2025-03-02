7928469c09f6f9b49df0e1d1e4e82d1a4f955e3b
Lucas <dev.lucas.atala@gmail.com>
2025-03-03 09:31:39 +1030

    Added cubic beizer and animation to hero section

--- src/sections/Main/Hero.tsx
+++ src/sections/Main/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <section className={`${styles.hero} container`}>
             <h2 className={styles.subtitle}>Welcome to</h2>
             <div className={styles.titleWrapper}>
-                <h1 className={styles.title}>ADELAIDE <br />ON WHEELS<br /> <span className={styles.lastTitleLine}>PRO</span></h1>
+                <h1 className={styles.title}>ADELAIDE <br />ON <br />WHEELS<br /> <span className={styles.lastTitleLine}>PRO</span></h1>
                 <Image src={Images.hero}
                     alt="inline skater turning"
                     className={styles.img} >
